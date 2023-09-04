23f47dcc21398780d34abaa21579afce32515c13
yanyanran <yanran66666@qq.com>
2023-09-04 19:25:09 +0800

    完善coordinator启动流程:添加读取configFile和创建git_tree

--- coordinator/server.py
+++ coordinator/server.py
@@ -1,11 +1,15 @@
 from concurrent import futures
 import time
 import grpc
+import os
 import threading
 import sys
 sys.path.append('../protocal/')
 import func_pb2
 import func_pb2_grpc
+import yaml
+import pathlib
+import subprocess
 from bitarray import bitarray
 from dataclasses import dataclass
 
@@ -19,7 +23,7 @@ class Worker:
 BITMAP_MAX_NUM = 1000   # 管理worker的bitmap默认大小（TODO：从配置文件读/写死）
 HEARTBEAT_INTERVAL = 10
 
-workers = {}  # 存放所有worker
+workers = {}  # 存放所有worker（TODO：cache持久化）
 bitmap = bitarray(BITMAP_MAX_NUM)
 
 # bitmap申请空闲id 
@@ -69,10 +73,85 @@ def dealTimeout():
                 release_id(bitmap, v.worker_id)
                 workers.pop(v.worker_id)    # TODO: worker全局不delete
         time.sleep(5)
-                
-            
+    
+# 读取配置文件               
+def read_config_file():
+    curPath = os.path.dirname(os.path.realpath(__file__))
+    yamlPath = os.path.join(curPath, "config.yaml")
+    
+    with open(yamlPath, 'r', encoding='utf-8') as f:
+        config = f.read()
+        
+    configMap = yaml.load(config,Loader=yaml.FullLoader)
+    repos_name=configMap.get('upstream_repos_name')
+    repos_url=configMap.get('upstream_repos_url')
 
-def serve():
+    # 获取绝对路径
+    reposPath = os.path.join(curPath, repos_name)
+    while True:
+        if os.path.exists(reposPath) == False:
+            print("文件不存在！")
+            command = "git clone %s" % repos_url
+            result = subprocess.run(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True)
+            if result.returncode != 0:
+                if std_neterr(result.stderr):
+                    print("upstream仓库克隆失败.... ")
+                    time.sleep(2)
+                    continue
+            print("upstream仓库克隆成功! ")
+        else:
+            print("文件存在！")
+            command = "cd %s && git pull" % reposPath
+            result = subprocess.run(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True)
+            # git pull错误情况解析判断
+            # 1、网断：隔一段时间反复重试
+            # 2、git_url失效：删除原来的本地仓库、重新读取配置文件获取最新url、重新clone
+            if result.returncode != 0:
+                if std_neterr(result.stdout):
+                    continue
+                else:
+                    os.remove(reposPath)
+                    continue
+       
+        return configMap
+
+# 判断网络连接问题
+def std_neterr(stderr):
+    err='failed: The TLS connection was non-properly terminated.'
+    if err in stderr :
+        return True
+    else :
+        return False 
+       
+def build_git_tree(configMap):
+    git_tree = {}
+    curPath = os.path.dirname(os.path.realpath(__file__))
+    reposPath = os.path.join(curPath, configMap.get('upstream_repos_name'))
+    
+    for i in range(26):
+        letter = chr(i + 97)
+        child_reposPath = os.path.join(reposPath, letter)
+        # print(child_reposPath)
+        dir_path = pathlib.Path(child_reposPath) # 指定要遍历的文件目录路径
+        
+        repo_tree = {}
+        git_tree[letter] = repo_tree
+        for item in dir_path.rglob('*'):
+            if item.is_dir():
+                repoPath = os.path.join(child_reposPath, item.name)  # b/bis
+                repoPath = os.path.join(repoPath, item.name)  # b/bis/bis
+                with open(repoPath, 'r', encoding='utf-8') as f:
+                    repo = f.read()
+                    repoMap = yaml.load(repo,Loader=yaml.FullLoader)
+                    repo_tree[item.name] = repoMap.get("url")
+    return git_tree           
+    
+def startup():
+    configMap = read_config_file()
+    git_tree = build_git_tree(configMap)
+    print(git_tree.get("a").get("abc"))
+
+def rpc_serve():
     # 启动 rpc 服务
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
     func_pb2_grpc.add_CoordinatorServicer_to_server(Coordinator(), server)
@@ -83,6 +162,10 @@ def serve():
             time.sleep(60*60*24) # one day in seconds
     except KeyboardInterrupt:
         server.stop(0)
+
+def serve():
+    startup()
+    rpc_serve()
 
 if __name__ == '__main__':
     heartbeat=threading.Thread(target=dealTimeout)
